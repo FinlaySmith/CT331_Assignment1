cdb75daf72393027932ee10e9716d3a2e967f254
Alan Flaherty <alanflaherty@mail.com>
2018-10-25 18:27:16 +0100

    Assignment1 - Question 1, Complete Code

--- src/q1/assignment.c
+++ src/q1/assignment.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
 int main(int arg, char* argc[]){
-  printf("Hello assignment1.\n");
+  printf("\n****************************************\n"); 
+  printf("** CT311 Assignment1 - Question 1     **\n");
+  printf("****************************************\n\n");
+
+  int i = 10;
+  int* i_ref = &i;
+  
+  double d = 2.0f;
+  double* d_ref = &d;
+
+  long int l = 4;
+  
+  char c = 'C';
+  char* c_ref = &c;
+
+  printf("  size of types:\n\n >> int     : %d bytes\n >> int*    : %d bytes\n >> long    : %d bytes\n >> double* : %d bytes\n >> char**  : %d bytes\n", 
+          sizeof(i), sizeof(i_ref), sizeof(l), sizeof(d_ref), sizeof(c_ref) );
+
   return 0;
 }
