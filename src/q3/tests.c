04c7f38a9778e708fdfbc8dd748c69474a80cb89
Alan Flaherty <alanflaherty@mail.com>
2018-10-26 02:16:43 +0100

    Assignment 1 - Question 3, expanded on basic tests

--- src/q3/tests.c
+++ src/q3/tests.c
@@ -9,18 +9,33 @@ int printInt(void* data)
 }
 
 void runTests(){
-  printf("Adding ints...\n");
-  int arr[] = {10, 20, 30, 40, 50}; 
-  genericListElement* item = createEl(&arr[0], sizeof(int), printInt);
+  printf("** Adding ints to generic linked list\n");
+  int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100}; 
+  genericListElement* head = createEl(&arr[0], sizeof(int), printInt);
 
-  genericListElement* insertionPoint = item;
+  genericListElement* insertionPoint = head;
   for (int i=1; i<=4; i++)
   {
     genericListElement* newItem = insertAfter(insertionPoint, &arr[i], sizeof(int), printInt); 
     insertionPoint = newItem;
   }
+  traverse(head);
 
-  traverse(item);
+  deleteAfter(head);
+  
+  printf("\n** Delete After head\n");
+  traverse(head);
+
+  push(&head, &arr[5], sizeof(int), printInt);
+  push(&head, &arr[6], sizeof(int), printInt);
+  pop(&head);
+
+  printf("\n** After sequence of push, push, pop\n");
+  traverse(head);
+
+  printf("\n** Dequeue tail and print\n");
+  genericListElement* tail = dequeue(head);
+  tail->printFunction(tail->data);
 
   printf("\nTests complete.\n");
 }
